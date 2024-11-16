3c9db5c82efef0d98e9ed00bf9fb229abc289ddd
EthanLDot <ethanlee2337@gmail.com>
2024-11-16 00:01:39 -0800

    Updated prettier pipeline to check, used prettier write
    Contributor: Ethan L.
    - Used prettier write on relevant source code to perform code formatting
    - changed the yml file to use the package.json script for prettier

--- frontend/preload.js
+++ frontend/preload.js
@@ -1,10 +1,9 @@
 // Preload script for security
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector);
-      if (element) element.innerText = text;
-    };
-  
-    replaceText('app-version', process.versions.electron);
-  });
-  
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  replaceText("app-version", process.versions.electron);
+});
