ca1604dbd70fb25c7169e2baf83c48e4d4645a18
EthanLDot <ethanlee2337@gmail.com>
2024-11-21 00:19:20 -0800

    feat(ui): added a simple login button
    Contributors: Ethan Lee
    - Added a simple login button
    - Non functional, modal + login interface is TBA
    - also fixed a minor bug in the lint and formatting yml file

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,12 +11,12 @@ export default [
       },
     },
     rules: {
-      "no-unused-vars": "error",    // Disallow unused variables
-      "no-undef": "error",          // Disallow undefined variables
-      semi: ["error", "always"],    // Enforce semicolons
-      eqeqeq: "error",              // Enforce strict equality (===)
-      "no-console": "warn"          // Warn about console.log usage (optional)
-    }
+      "no-unused-vars": "error", // Disallow unused variables
+      "no-undef": "error", // Disallow undefined variables
+      semi: ["error", "always"], // Enforce semicolons
+      eqeqeq: "error", // Enforce strict equality (===)
+      "no-console": "warn", // Warn about console.log usage (optional)
+    },
   },
   pluginJs.configs.recommended,
 ];
