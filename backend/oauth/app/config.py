c7573cb255d103b8e8d74a30df53da510524441d
nishant42491 <rajadhyakshanishant@gmail.com>
2024-11-24 11:32:15 -0800

    okay

--- backend/oauth/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load .env file
-load_dotenv()
-
-# Retrieve variables
-CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
-CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
