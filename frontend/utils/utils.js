7a4a1a7e1fb36172a9da76d570cb986aed89456a
EthanLDot <ethanlee2337@gmail.com>
2024-12-06 10:02:17 -0800

    FIX (core): Added prettier-write command, made code linting and code formatting compliant
    CONTRIBUTORS: Ethan L.
    - Added "prettier-write" command to automatically reformat code to match prettier guidelines.
    - Fixed some minor issues preventing eslint pipeline from passing

--- frontend/utils/utils.js
+++ frontend/utils/utils.js
@@ -1,18 +1,18 @@
 export function saveToLocalStorage(key, value) {
-	sessionStorage.setItem(key, value);
-	console.log(`Saved to local storage -> ${key}: ${value}`);
+  sessionStorage.setItem(key, value);
+  console.log(`Saved to local storage -> ${key}: ${value}`);
 
-	// sessionStorage.setItem('accessToken', token);
-	// console.log('Access token saved:', token);
+  // sessionStorage.setItem('accessToken', token);
+  // console.log('Access token saved:', token);
 }
 
 export function loadFromLocalStorage(key) {
-	const value = sessionStorage.getItem(key);
-	console.log(`Loaded from local storage -> ${key}: ${value}`);
-	return value;
+  const value = sessionStorage.getItem(key);
+  console.log(`Loaded from local storage -> ${key}: ${value}`);
+  return value;
 }
 
 export function deleteFromLocalStorage(key) {
-	sessionStorage.removeItem(key);
-	console.log(`Deleted from local storage -> ${key}`);
+  sessionStorage.removeItem(key);
+  console.log(`Deleted from local storage -> ${key}`);
 }
