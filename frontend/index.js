09853989f8fc323952a930e60ea672504ccc6a96
SushaanthSrinivasan <iamsushaanth@gmail.com>
2024-12-07 23:37:28 -0800

    feat(google-search): implemented google seacrh when user types in the journal

--- frontend/index.js
+++ frontend/index.js
@@ -2,11 +2,11 @@ const openSigninModalButton = document.getElementById("openSigninModal");
 const openSignupModalButton = document.getElementById("openSignupModal");
 
 openSigninModalButton.addEventListener("click", () => {
-	const modal = document.getElementById("signin-modal");
-	modal.dispatchEvent(new Event("open"));
+  const modal = document.getElementById("signin-modal");
+  modal.dispatchEvent(new Event("open"));
 });
 
 openSignupModalButton.addEventListener("click", () => {
-	const modal = document.getElementById("signup-modal");
-	modal.dispatchEvent(new Event("open"));
+  const modal = document.getElementById("signup-modal");
+  modal.dispatchEvent(new Event("open"));
 });
--- frontend/pages/home/home.js
+++ frontend/pages/home/home.js
@@ -1,26 +1,26 @@
-import { API_BASE_URL } from "../../constants/constants.js";
-import { loadFromSessionStorage } from "../../utils/utils.js";
+// import { API_BASE_URL } from "../../constants/constants.js";
+// import { loadFromSessionStorage } from "../../utils/utils.js";
 
 document.getElementById("create-journal-btn").addEventListener("click", () => {
-	// Navigate to a blank new journal (no parameters)
-	window.location.href = "../journal/journal.html";
+  // Navigate to a blank new journal (no parameters)
+  window.location.href = "../journal/journal.html";
 });
 
 document.getElementById("edit-journal-btn").addEventListener("click", () => {
-	// Navigate to an existing journal edit page, passing a parameter (e.g., journalId)
-	const journalId = document.getElementById("journal-id").value;
-	// console.log(journalId);
-	window.location.href = `../journal/journal.html?journalId=${journalId}`;
+  // Navigate to an existing journal edit page, passing a parameter (e.g., journalId)
+  const journalId = document.getElementById("journal-id").value;
+  // console.log(journalId);
+  window.location.href = `../journal/journal.html?journalId=${journalId}`;
 });
 
-document
-	.getElementById("search-journals-input")
-	.addEventListener("input", (event) => {
-		const searchTerm = event.target.value;
-		const queryParams = new URLSearchParams({
-			search_text: searchTerm,
-			auth_token: loadFromSessionStorage("accessToken"),
-		});
-		// TODO
-		// const response = fetch(`${${API_BASE_URL}}/search`, {
-	});
+// document
+// 	.getElementById("search-journals-input")
+// 	.addEventListener("input", (event) => {
+// 		const searchTerm = event.target.value;
+// 		const queryParams = new URLSearchParams({
+// 			search_text: searchTerm,
+// 			auth_token: loadFromSessionStorage("accessToken"),
+// 		});
+// 		// TODO
+// 		// const response = fetch(`${${API_BASE_URL}}/search`, {
+// 	});
