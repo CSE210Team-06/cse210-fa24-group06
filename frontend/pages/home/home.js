9f2313365d880583ea83e6798788771494d9423f
uloulou <ulou@ucsd.edu>
2024-12-09 18:18:00 -0800

    update UI + User name

--- frontend/pages/home/home.js
+++ frontend/pages/home/home.js
@@ -32,19 +32,3 @@ document.getElementById("secrets-btn").addEventListener("click", () => {
 // 		// const response = fetch(`${${API_BASE_URL}}/search`, {
 // 	});
 
-async function getData() {
-	const url = "'http://localhost:3000/get_user/user_details";
-	try {
-	  const response = await fetch(url);
-	  if (!response.ok) {
-		throw new Error(`Response status: ${response.status}`);
-	  }
-  
-	  const json = await response.json();
-	  console.log(json);
-	} catch (error) {
-	  console.error(error.message);
-	}
-  }
-
-getData();
