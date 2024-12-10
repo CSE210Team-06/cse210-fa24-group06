3fb21e97e8aeecca1fee9f974589265b23abd511
EthanLDot <ethanlee2337@gmail.com>
2024-12-09 17:30:09 -0800

    Feat(ui): added mock interface for journal groups
    CONTRIBUTORS: Ethan L.
    - created a simple dropdown that is populated with group names from a sample json file.
    - TODO: connect with our existing backend when appropriate endpoints are made. Make sure that the journals listed below will actually match that belonging to the group.

--- frontend/pages/home/home.js
+++ frontend/pages/home/home.js
@@ -1,13 +1,16 @@
-// import { API_BASE_URL } from "../../constants/constants.js";
-// import { loadFromSessionStorage } from "../../utils/utils.js";
-// import { API_BASE_URL } from "../../constants/constants.js";
-// import { loadFromSessionStorage } from "../../utils/utils.js";
-
+/**
+ * Event listener for "Create Journal" button.
+ * Navigates to a blank new journal page.
+ */
 document.getElementById("create-journal-btn").addEventListener("click", () => {
   // Navigate to a blank new journal (no parameters)
   window.location.href = "../journal/journal.html";
 });
 
+/**
+ * Event listener for "Edit Journal" button.
+ * Navigates to an existing journal edit page, passing the journalId as a query parameter.
+ */
 document.getElementById("edit-journal-btn").addEventListener("click", () => {
   // Navigate to an existing journal edit page, passing a parameter (e.g., journalId)
   const journalId = document.getElementById("journal-id").value;
@@ -15,19 +18,50 @@ document.getElementById("edit-journal-btn").addEventListener("click", () => {
   window.location.href = `../journal/journal.html?journalId=${journalId}`;
 });
 
+/**
+ * Event listener for "Secrets" button.
+ * Navigates to the secrets page.
+ */
 document.getElementById("secrets-btn").addEventListener("click", () => {
-  // console.log("Secrets button clicked");
   window.location.href = "../secrets/secrets.html";
 });
 
-// document
-// 	.getElementById("search-journals-input")
-// 	.addEventListener("input", (event) => {
-// 		const searchTerm = event.target.value;
-// 		const queryParams = new URLSearchParams({
-// 			search_text: searchTerm,
-// 			auth_token: loadFromSessionStorage("accessToken"),
-// 		});
-// 		// TODO
-// 		// const response = fetch(`${${API_BASE_URL}}/search`, {
-// 	});
+const dropdown = document.getElementById("journal-group-select");
+/**
+ * Populates a dropdown menu with group data.
+ *
+ * @param {Object} data - The data object containing groups.
+ * @param {Array} data.groups - An array of group objects.
+ * @param {string} data.groups[].id - The unique identifier for the group.
+ * @param {string} data.groups[].name - The name of the group.
+ */
+function populateDropdown(data) {
+  data.groups.forEach((group, index) => {
+    const option = document.createElement("option");
+    option.value = group.id;
+    option.textContent = group.name;
+    dropdown.appendChild(option);
+
+    // Select the first group by default
+    if (index === 0) {
+      dropdown.value = group.id;
+    }
+  });
+}
+
+/**
+ * Fetches group data from a mock database and populates the dropdown menu.
+ */
+fetch("../../mock-db/groups.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    populateDropdown(data);
+  })
+  .catch((error) => {
+    console.error("Error loading JSON data:", error);
+  });
